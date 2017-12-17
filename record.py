d51d41b40afaa5843536288b3f0ce555f34b0dfc
SaeedRafay <saeedahmadrafay@hotmail.com>
2017-12-17 15:33:08 +0500

    Prints the records line by line in reverse order

--- record.py
+++ record.py
@@ -3,23 +3,23 @@ import csv
 
 class Record:
 
-    def save_file(self, student_id, first_name, last_name):
-        try:
-            with open("students.csv", "a", newline="\n", encoding="utf-8") as inp:
-                writer = csv.writer(inp)
-                writer.writerow([student_id, first_name, last_name])
-        except Exception:
-            print("Could not save file")
-
     def read_file(self):
         students = []
         try:
             with open("students.csv", "r") as out:
                 for row in csv.reader(out):
                     students.append(row)
-            print(students)
+            print(*students[::-1], sep='\n')
         except Exception:
             print("Could not read file")
+
+    def save_row(self, student_id, first_name, last_name):
+        try:
+            with open("students.csv", "a", newline="\n", encoding="utf-8") as inp:
+                writer = csv.writer(inp)
+                writer.writerow([student_id, first_name, last_name])
+        except Exception:
+            print("Could not save file")
 
     def delete_row(self, student_id):
         students = []
@@ -28,7 +28,7 @@ class Record:
                 for row in csv.reader(out):
                     if row[0] != student_id:
                         students.append(row)
-            print(students)
+            print(*students[::-1], sep='\n')
             with open("students.csv", "w", newline="\n", encoding="utf-8") as inp:
                 writer = csv.writer(inp)
                 for student in students:
